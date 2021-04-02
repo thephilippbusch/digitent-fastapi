9097d8fd4cd7134caa61e350aa8f3040299b48b1
Philipp Busch <philipp-leon-busch@hotmail.de>
2021-04-02 22:08:26 +0200

    small change again

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ client = MongoClient(host="localhost", port=27017)
 db = client["chat_bot"]
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host='0.0.0.0')
