37f03555f7df6511cde99a66a32825d4a6100e95
Philipp Busch <philipp-leon-busch@hotmail.de>
2021-04-08 23:09:26 +0200

    rdy for deploy

--- app/postgres/postgres.py
+++ app/postgres/postgres.py
@@ -1,18 +1,17 @@
 import psycopg2, psycopg2.extras
 from decouple import config
 import uuid
+import os
 
-HOST = config("postgres_host")
-PORT = config("postgres_port")
-DB = config("postgres_database")
-USER = config("postgres_user")
+# HOST = config("postgres_host")
+# PORT = config("postgres_port")
+# DB = config("postgres_database")
+# USER = config("postgres_user")
 
-conn = psycopg2.connect(
-    host=HOST,
-    port=PORT,
-    database=DB,
-    user=USER
-)
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 init_commands = (
